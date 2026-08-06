51256e63f4a08c747386f06863888b8409751a8b
Godwill <agoraproject.godwinn@gmail.com>
2026-08-06 21:28:55 +0100

    V2 DEVINE LE NOMBRE

--- week1/projet1.py
+++ week1/projet1.py
@@ -5,7 +5,7 @@ print("Bienvenue dans le jeu du devine le nombre !")
 levels = ["Facile (0-20)", "Moyen (0-50)", "Difficile (0-100)"]
 for index, level in enumerate(levels):
     print(f"{index + 1}. {level}")
-    
+
 level = int(input("Choisis un niveau : "))
 if level == 1:
     a = random.randint(0, 20)
@@ -14,9 +14,17 @@ elif level == 2:
 else:
     a = random.randint(0, 100)
 
-b = int(input("Devine le nombre aléatoire : "))
-
 while True:
+
+    b = int(input("Devine le nombre aléatoire : "))
+
+    if level == 1 and (b < 0 or b > 20):
+        print("Le nombre que tu as choisi est invalide.")
+    elif level == 2 and (b < 0 or b > 50):
+        print("Le nombre que tu as choisi est invalide.")
+    elif level == 3 and (b < 0 or b > 100):
+        print("Le nombre que tu as choisi est invalide.")
+    
     if b == a:
         print("Félicitations ! Tu as deviné le nombre.")
 
@@ -36,21 +44,13 @@ while True:
             print("Merci d'avoir joué !")
             break
 
-    elif a - 5 <= b <= a + 5:
+    elif a - 5 <= b <= a + 5 and ((level == 1 and not (b < 0 or b > 20)) or (level == 2 and not (b < 0 or b > 50)) or (level == 3 and not (b < 0 or b > 100))):
         print("Le nombre que tu as choisi est très proche.")
-    elif a + 5 < b <= a + 20:
+    elif a + 5 < b <= a + 20 and ((level == 1 and not (b < 0 or b > 20)) or (level == 2 and not (b < 0 or b > 50)) or (level == 3 and not (b < 0 or b > 100))):
         print("Le nombre que tu as choisi est supérieur.")
-    elif a - 20 <= b < a - 5:
+    elif a - 20 <= b < a - 5 and ((level == 1 and not (b < 0 or b > 20)) or (level == 2 and not (b < 0 or b > 50)) or (level == 3 and not (b < 0 or b > 100))):
         print("Le nombre que tu as choisi est inférieur.")
-    elif a + 20 < b <= 100:
+    elif a + 20 < b <= 100 and ((level == 1 and not (b < 0 or b > 20)) or (level == 2 and not (b < 0 or b > 50)) or (level == 3 and not (b < 0 or b > 100))):
         print("Le nombre que tu as choisi est trop grand.")
-    elif 0 <= b < a - 20:
+    elif 0 <= b < a - 20 and ((level == 1 and not (b < 0 or b > 20)) or (level == 2 and not (b < 0 or b > 50)) or (level == 3 and not (b < 0 or b > 100))):
         print("Le nombre que tu as choisi est trop petit.")
-    elif level == 1 and (b < 0 or b > 20):
-        print("Le nombre que tu as choisi est invalide.")
-    elif level == 2 and (b < 0 or b > 50):
-        print("Le nombre que tu as choisi est invalide.")
-    elif level == 3 and (b < 0 or b > 100):
-        print("Le nombre que tu as choisi est invalide.")
-
-    b = int(input("Devine le nombre aléatoire : "))
